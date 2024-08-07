f5bb431932a546f42b6489f5b324458a79da25d4
Aaron Berg <aaron6235@gmail.com>
2024-08-07 14:17:03 -0600

    able to fix client not live updating, client talking to api and database on InventoryList

--- client/src/components/InventoryList.js
+++ client/src/components/InventoryList.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/InventoryList.css';
 
 const InventoryList = () => {
-  const items = [
-    { id: 1, name: 'Nails', quantity: 10, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id eleifend nibh. Integer et aliquam odio. In a porttitor dolor, vel maximus dui. In in ullamcorper eros. Morbi turpis odio, vulputate at sagittis eu, consectetur vel lorem. Pellentesque ac ullamcorper sem, id laoreet nulla. Fusce euismod aliquet justo, aliquam varius sapien viverra nec. Pellentesque eget metus arcu. Sed et magna luctus, congue felis nec, cursus magna. Proin malesuada blandit dictum. Maecenas massa dolor, viverra quis gravida a, ornare a diam. Nullam blandit blandit ligula sed pulvinar. Aliquam a nibh volutpat, sollicitudin enim eget, malesuada sapien. Aliquam et tristique velit. Quisque.' },
-    { id: 2, name: 'Bolts', quantity: 5, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id eleifend nibh. Integer et aliquam odio. In a porttitor dolor, vel maximus dui. In in ullamcorper eros. Morbi turpis odio, vulputate at sagittis eu, consectetur vel lorem. Pellentesque ac ullamcorper sem, id laoreet nulla. Fusce euismod aliquet justo, aliquam varius sapien viverra nec. Pellentesque eget metus arcu. Sed et magna luctus, congue felis nec, cursus magna. Proin malesuada blandit dictum. Maecenas massa dolor, viverra quis gravida a, ornare a diam. Nullam blandit blandit ligula sed pulvinar. Aliquam a nibh volutpat, sollicitudin enim eget, malesuada sapien. Aliquam et tristique velit. Quisque.' },
-    { id: 3, name: 'Screws', quantity: 100, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id eleifend nibh. Integer et aliquam odio. In a porttitor dolor, vel maximus dui. In in ullamcorper eros. Morbi turpis odio, vulputate at sagittis eu, consectetur vel lorem. Pellentesque ac ullamcorper sem, id laoreet nulla. Fusce euismod aliquet justo, aliquam varius sapien viverra nec. Pellentesque eget metus arcu. Sed et magna luctus, congue felis nec, cursus magna. Proin malesuada blandit dictum. Maecenas massa dolor, viverra quis gravida a, ornare a diam. Nullam blandit blandit ligula sed pulvinar. Aliquam a nibh volutpat, sollicitudin enim eget, malesuada sapien. Aliquam et tristique velit. Quisque.' },
-  ];
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const baseUrl = `http://localhost:8080/items`;
+
+    fetch(baseUrl)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Error getting Network Response');
+        }
+        return res.json();
+      })
+      .then(data => {
+        setItems(data);
+      })
+      .catch(error => {
+        console.error('The fetch did not work', error)
+      });
+}, [])
+
+
+// const InventoryList = () => {
+//   const items = [
+//     { id: 1, name: 'Nails', quantity: 10, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id eleifend nibh. Integer et aliquam odio. In a porttitor dolor, vel maximus dui. In in ullamcorper eros. Morbi turpis odio, vulputate at sagittis eu, consectetur vel lorem. Pellentesque ac ullamcorper sem, id laoreet nulla. Fusce euismod aliquet justo, aliquam varius sapien viverra nec. Pellentesque eget metus arcu. Sed et magna luctus, congue felis nec, cursus magna. Proin malesuada blandit dictum. Maecenas massa dolor, viverra quis gravida a, ornare a diam. Nullam blandit blandit ligula sed pulvinar. Aliquam a nibh volutpat, sollicitudin enim eget, malesuada sapien. Aliquam et tristique velit. Quisque.' },
+//     { id: 2, name: 'Bolts', quantity: 5, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id eleifend nibh. Integer et aliquam odio. In a porttitor dolor, vel maximus dui. In in ullamcorper eros. Morbi turpis odio, vulputate at sagittis eu, consectetur vel lorem. Pellentesque ac ullamcorper sem, id laoreet nulla. Fusce euismod aliquet justo, aliquam varius sapien viverra nec. Pellentesque eget metus arcu. Sed et magna luctus, congue felis nec, cursus magna. Proin malesuada blandit dictum. Maecenas massa dolor, viverra quis gravida a, ornare a diam. Nullam blandit blandit ligula sed pulvinar. Aliquam a nibh volutpat, sollicitudin enim eget, malesuada sapien. Aliquam et tristique velit. Quisque.' },
+//     { id: 3, name: 'Screws', quantity: 100, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id eleifend nibh. Integer et aliquam odio. In a porttitor dolor, vel maximus dui. In in ullamcorper eros. Morbi turpis odio, vulputate at sagittis eu, consectetur vel lorem. Pellentesque ac ullamcorper sem, id laoreet nulla. Fusce euismod aliquet justo, aliquam varius sapien viverra nec. Pellentesque eget metus arcu. Sed et magna luctus, congue felis nec, cursus magna. Proin malesuada blandit dictum. Maecenas massa dolor, viverra quis gravida a, ornare a diam. Nullam blandit blandit ligula sed pulvinar. Aliquam a nibh volutpat, sollicitudin enim eget, malesuada sapien. Aliquam et tristique velit. Quisque.' },
+//   ];
 
   return (
     <div>
@@ -33,4 +55,5 @@ const InventoryList = () => {
 };
 
 export default InventoryList;
+
 
