6871303ed48a82f6ed8fbf9beade0d233ebea1a9
Aaron Berg <aaron6235@gmail.com>
2024-08-06 18:51:51 -0600

    Verified full connection from database to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Inventory</h1>
+//       {/* <Board/> */}
+//       {/* <GameBoard size={8} /> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+import { useEffect, useState } from "react";
+const baseUrl = `http://localhost:8080/users`;
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Inventory</h1>
-      {/* <Board/> */}
-      {/* <GameBoard size={8} /> */}
-    </div>
-  );
+  let [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return <div className="App">
+    {JSON.stringify(users)}
+  </div>
 }
 
 export default App;
